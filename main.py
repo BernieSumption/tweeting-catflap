dc63e6ac1da2f61c0d12f45d567ff19152985482
Bernie Sumption <bernie@berniecode.com>
2013-10-01 21:08:47 +0100

    Swtiched to using twython for twitter posting

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from gpio_watcher import GPIOWatcher
 from subprocess import call
 from datetime import datetime
 from grammar import Grammar
-import os, shutil
+import os
+
+from twython import Twython
+import sys
 
 def goGoPaparazzo():
     
@@ -26,9 +29,14 @@ def goGoPaparazzo():
     message = grammar.generate()
 
     # post to twitter
-    safeMessage = message.replace("\\", "\\\\").replace("\"", "\\\"")
-    call(["./tweet.sh \"%s\"" % safeMessage], shell=True)
-    
+	twitter = Twython(
+		app_key = 'uS6hO2sV6tDKIOeVjhnFnQ',
+		app_secret = 'MEYTOS97VvlHX7K1rwHPEqVpTSqZ71HtvoK4sVuYk',
+		oauth_token = '1334844578-z3Ju3FUAQKZKogQnK7kbqngeeSQxX1wkeGwRiey',
+		oauth_token_secret = 'HD1w0jh2x2nxAcgPI6Cux1SKbxI0VxaQYHvNZn8dGxQ'
+	)
+	twitter.update_status_with_media(status=message, media=open("capture.jpg", 'rb'))
+
     # archive the image and text
     shutil.move("capture.jpg", "history/%s.jpg" % timestamp)
     with open("history/%s.txt" % timestamp, "w") as f:
